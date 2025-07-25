d412b113b9d20dd58d9a0990be46c08d54a9e3fe
Vidushi Kataria <vk412@scarletmail.rutgers.edu>
2025-07-25 15:34:30 -0400

    Update statc/app.js

--- static/app.js
+++ static/app.js
@@ -1,151 +1,97 @@
 class Chatbox {
-    constructor() {
-        this.args = {
-            openButton: document.querySelector('.chatbox__button'),
-            chatBox: document.querySelector('.chatbox__support'),
-            sendButton: document.querySelector('.send__button')
-        }
+  constructor() {
+    this.args = {
+      openButton: document.querySelector('.chatbox__button'),
+      chatBox:   document.querySelector('.chatbox__support'),
+      sendButton: document.querySelector('.chatbox__send--footer')
+    };
+    this.state = false;
+    this.messages = [];
+  }
 
-        this.state = false;
-        this.messages = [];
-    }
+  display() {
+    const { openButton, chatBox, sendButton } = this.args;
 
-    display() {
-        const {openButton, chatBox, sendButton, medicalcentres} = this.args;
+    openButton.addEventListener('click', () => this.toggleState(chatBox));
 
-        openButton.addEventListener('click', () => this.toggleState(chatBox))
+    sendButton.addEventListener('click', () => this.onSendButton(chatBox));
 
-        sendButton.addEventListener('click', () => this.onSendButton(chatBox))
+    const inputNode = chatBox.querySelector('input');
+    inputNode.addEventListener('keyup', ({ key }) => {
+      if (key === 'Enter') {
+        this.onSendButton(chatBox);
+      }
+    });
+  }
 
-        const node = chatBox.querySelector('input');
-        node.addEventListener("keyup", ({key}) => {
-            if (key === "Enter") {
-                this.onSendButton(chatBox)
-            }
-        })
-    }
+  toggleState(chatbox) {
+    this.state = !this.state;
+    chatbox.classList.toggle('chatbox--active', this.state);
+  }
 
-    toggleState(chatbox) {
-        this.state = !this.state;
+  onSendButton(chatbox) {
+    const textField = chatbox.querySelector('input');
+    const userText = textField.value.trim();
+    if (!userText) return;
 
-        // show or hides the box
-        if(this.state) {
-            chatbox.classList.add('chatbox--active')
-        } else {
-            chatbox.classList.remove('chatbox--active')
-        }
-    }
+    // 1) push user message
+    this.messages.push({ name: 'User', message: userText });
+    this.updateChatText(chatbox);
+    textField.value = '';
 
-    onSendButton(chatbox) {
-        var textField = chatbox.querySelector('input');
-        let text1 = textField.value
-        if (text1 === "") {
-            return;
-        }
+    // 2) call backend
+    fetch(`${window.SCRIPT_ROOT}/get_response`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ message: userText })
+    })
+    .then(res => res.json())
+    .then(([ tag, responses, precautionOrCenters ]) => {
+      // tag: intent tag
+      // responses: array of response strings
+      // precautionOrCenters: may be string or array (centers list)
+      // push bot message(s)
+      this.messages.push({ name: 'Sam', message: responses[0] });
+      if (precautionOrCenters) {
+        this.messages.push({ name: 'Sam', message: precautionOrCenters });
+      }
+      this.updateChatText(chatbox);
+    })
+    .catch(err => {
+      console.error(err);
+      this.messages.push({ name: 'Sam', message: "Sorry, something went wrong." });
+      this.updateChatText(chatbox);
+    });
+  }
 
-        let msg1 = { name: "User", message: text1 }
-        this.messages.push(msg1);
+  updateChatText(chatbox) {
+    const container = chatbox.querySelector('.chatbox__messages > div');
+    container.innerHTML = ''; // clear
 
-        fetch('http://127.0.0.1:5000/predict', {
-            method: 'POST',
-            body: JSON.stringify({ message: text1}),
-            mode: 'cors',
-            headers: {
-               'Content-Type': 'application/json'
-            },
-          })
-          .then(r => r.json())
-          .then(r => {
-            //let msg2 = { name: "Sam", message1: r.answer[0][0], message11: r.answer[0][1], prob1: r.answer[0][2], message2: r.answer[1][0], message21: r.answer[1][1], prob2: r.answer[1][2], message3: r.answer[2][0], message31: r.answer[2][1], prob3: r.answer[2][2]};
-            let msg2 = {name: "Sam", message1: r.answer[0], message2: r.answer[1], message3: r.answer[2], message4: r.answer[3]}
-            this.messages.push(msg2);
-            this.updateChatText(chatbox)
-            textField.value = ''
+    // render each message
+    this.messages.forEach(item => {
+      const msgEl = document.createElement('div');
+      const whoClass = item.name === 'Sam'
+        ? 'messages__item--operator'
+        : 'messages__item--visitor';
 
-        }).catch((error) => {
-            console.error('Error:', error);
-            this.updateChatText(chatbox)
-            textField.value = ''
-        });
-    }
+      msgEl.classList.add('messages__item', whoClass);
+      msgEl.textContent = item.message;
+      container.appendChild(msgEl);
+    });
 
-    updateChatText(chatbox) {
-        var html = '';
-        const specificTags = ["greeting", "goodbye","work","who","Thanks","joke", "name", "age", "gender", "not_understand"]
-        this.messages.slice().reverse().forEach(function(item, index) {
-            
-            if (item.name === "Sam")
-            {
-                /*html +=  '<div class="myDIV">' + item.message3 + item.prob3 + '</div>' 
-                    + '<div class="hide">' + item.message31 +'</div>'
-                    + '<div class="myDIV">' + item.message2 + item.prob2 + '</div>'
-                    + '<div class="hide">' + item.message21 + '</div>'
-                    + '<div class="myDIV">' + item.message1 + item.prob1 + '</div>'
-                    + '<div class="hide">' + item.message11 + '</div>'
-                    + '<div class="con" style="margin-top:20px; margin-bottom:10px"><h3>These may be the possible diseases that you may have.</h3></div>'
-                */
-                if (specificTags.includes(item.message1)){
-                    html += '<div class="messages__item messages__item--visitor">' + item.message2 + '</div>'
-                }
-                else if (item.message1 === "center"){ 
-                    html += '<div class="messages__item messages__item--visitor">You can ask me if you want any thing else.</div>'
-                            + '<div class="myDIV" style="font-size: 17px;">' + item.message4[0] + ', ' + item.message4[1] + '</div>'
-                            + '<div class="hide">' + item.message4[2] + '</div>'
-                            + '<div class="myDIV" style="font-size: 17px;">' + item.message3[0] + ', ' + item.message3[1] + '</div>'
-                            + '<div class="hide">' + item.message3[2] + '</div>'
-                            + '<div class="myDIV" style="font-size: 17px;">' + item.message2[0] + ', ' + item.message2[1] + '</div>'
-                            + '<div class="hide">' + item.message2[2] + '</div>'
-                            + '<div class="con" style="margin-top:20px; margin-bottom:10px"><h3>Medical location that are near to you are.</h3></div>'
-                }
-                else{
-
-                    html += '<div class="messages__item messages__item--visitor">Do you want to know about the nearby medical center locations</div>'
-                            + '<div class="accordion" id="accordionExample">'
-                                + '<div class="accordion-item" style="width: 40%; margin-top: 10px" >'
-                                + '<h2 class="accordion-header" id="headingOne">'
-                                + '<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">'
-                                + '<b>Precautions</b>'
-                                + '</button>'
-                                + '</h2>'
-                                + '<div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">'
-                                + '<div class="accordion-body">'
-                                + item.message3
-                                + '</div>'
-                                + '</div>'
-                                + '</div>'
-                                + '</div>'
-                            + '<div class="accordion" id="accordionExample">'
-                                    + '<div class="accordion-item" style="width: 40%; margin-top: 10px" >'
-                                    + '<h2 class="accordion-header" id="headingThree">'
-                                    + '<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">'
-                                    + '<b>Description</b>'
-                                    + '</button>'
-                                    + '</h2>'
-                                    + '<div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">'
-                                    + '<div class="accordion-body">'
-                                    + item.message2
-                                    + '</div>'
-                                    + '</div>'
-                                    + '</div>'
-                                    + '</div>'
-                            + '<div class="messages__item messages__item--visitor">Here  is some more info on the disease</div>'
-                            + '<div class="myDIV">' + item.message1 + '</div>'
-                            + '<div class="con" style="margin-top:20px; margin-bottom:10px"><h3>This may be the possible disease that you may have.</h3></div>'   
-                }
-            }
-            else
-            {
-                html += '<div class="messages__item messages__item--operator">' + item.message + '</div>'
-            }
-          });
-          html += '<div class="messages__item messages__item--visitor">Hi, this is a medical chat support.</div><div class="messages__item messages__item--visitor">May I know your name.</div>'
-
-        const chatmessage = chatbox.querySelector('.chatbox__messages');
-        chatmessage.innerHTML = html;
-    }
-    
+    // scroll to bottom
+    container.scrollTop = container.scrollHeight;
+  }
 }
 
-
+// init
 const chatbox = new Chatbox();
 chatbox.display();
+
+// kick off first prompt:
+chatbox.messages.push({
+  name: 'Sam',
+  message: 'Hi, this is a medical chatbot. What is your age and gender?'
+});
+chatbox.updateChatText(chatbox.args.chatBox);
